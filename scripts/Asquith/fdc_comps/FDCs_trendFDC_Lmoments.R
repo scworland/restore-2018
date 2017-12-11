c81c6e7e5b2524991858bdbb91e3f53a1aeeabd5
William Asquith <33225874+wasquith@users.noreply.github.com>
2017-12-11 11:43:12 -0600

    updating after core decadal data review

--- scripts/Asquith/fdc_comps/FDCs_trendFDC_Lmoments.R
+++ scripts/Asquith/fdc_comps/FDCs_trendFDC_Lmoments.R
@@ -4,9 +4,9 @@ load("DV.RData")
 
 TFDC <- new.env()
 n <- fill_tfdcenv(dvenv=DV, envir=TFDC) # number fdctrend() processed
-AllFDCtrend <- visFDCtrend(TFDC, file="fdc_trend.pdf")
+AllFDCtrend <- visFDCtrend(TFDC, file="all_fdc_trend.pdf")
 save(AllFDCtrend, TFDC, file="AllFDCtrend.RData")
-#visFDCtrend(TFDC$"08093500") # new feature 09/28/2017
+#visFDCtrend(TFDC$"08093500", site="08093500") # new feature 09/28/2017
 
 # 12/01/2017
 sitesWOcompleteFDC <- c("02295163", "02300082", "02388350", "02432500",
@@ -14,9 +14,9 @@ sitesWOcompleteFDC <- c("02295163", "02300082", "02388350", "02432500",
 
 
 FDCLMRlog <- new.env()
-fill_lmrfdcenv(dvenv=DV, envir=FDCLMRlog, log=TRUE)
+fill_lmrfdcenv(dvenv=DV, envir=FDCLMRlog,   log=TRUE,  todecade=TRUE)
 FDCLMRnolog <- new.env()
-fill_lmrfdcenv(dvenv=DV, envir=FDCLMRnolog, log=FALSE)
+fill_lmrfdcenv(dvenv=DV, envir=FDCLMRnolog, log=FALSE, todecade=TRUE)
 
 FDClmrdf.log   <- lmrfdc_table(FDCLMRlog)
 FDClmrdf.nolog <- lmrfdc_table(FDCLMRnolog)
@@ -25,38 +25,76 @@ save(FDCLMRlog, FDCLMRnolog, FDClmrdf.log, FDClmrdf.nolog, file="FDCLMR.RData")
 load("FDCLMR.RData")
 
 pdf("lmrdia.pdf", useDingbats=FALSE)
-  L <- FDClmrdf.log
-  L$color <- rgb(.5,.3,0,.3)
-  L$color[L$nzero > 30 ] <- rgb(.3, 0.8, 0.1, 0.5) # greenish towards
-  L$color[L$nzero > 60 ] <- rgb(.3, 0.6, 0.2, 0.5)
-  L$color[L$nzero > 90 ] <- rgb(.3, 0.4, 0.3, 0.5)
-  L$color[L$nzero > 120] <- rgb(.3, 0.2, 0.4, 0.5)
-  L$color[L$nzero > 180] <- rgb(.3, 0,.5,0.5) # purple
+  L <- FDClmrdf.log; N <- FDClmrdf.nolog
+  L$color <- N$color <- rgb(.5,.3,0,.3) # zeros will go greenish towards purple
+  L$color[L$nzero > 30 ] <- N$color[L$nzero > 30 ] <- rgb(.3,.8,.1,.5) # green
+  L$color[L$nzero > 60 ] <- N$color[L$nzero > 60 ] <- rgb(.3,.6,.2,.5)
+  L$color[L$nzero > 90 ] <- N$color[L$nzero > 90 ] <- rgb(.3,.4,.3,.5)
+  L$color[L$nzero > 120] <- N$color[L$nzero > 120] <- rgb(.3,.2,.4,.5)
+  L$color[L$nzero > 180] <- N$color[L$nzero > 180] <- rgb(.3,.0,.5,.5) # purple
 
-  plotlmrdia(lmrdia(), xlim=c(-.6,.9), ylim=c(-.2,0.6))
+  plotlmrdia(lmrdia(), xlim=c(-.5,.9), ylim=c(-.1,0.6))
   points(L$T3, L$T4, cex=0.3, col=L$color, pch=16, lwd=0.3)
   mtext("log-transform of (DV+1cfs)")
 
-  N <- FDClmrdf.nolog
-  plotlmrdia(lmrdia(), xlim=c(-.4,1), ylim=c(-.2,1))
-  points(N$T3, N$T4, cex=0.3, col=rgb(.5,.3,0,.3), pch=16, lwd=0.3)
+  plotlmrdia(lmrdia(), xlim=c(-.05,1), ylim=c(0,1))
+  points(N$T3, N$T4, cex=0.3, col=N$color, pch=16, lwd=0.3)
   mtext("no transformation")
 dev.off()
 
+modFDClmrdf.log <- FDClmrdf.log[! is.na(FDClmrdf.log$site), ]
+modFDClmrdf.log <- modFDClmrdf.log[! is.na(FDClmrdf.log$min),  ]
+modFDClmrdf.log <- modFDClmrdf.log[FDClmrdf.log$decade >= 1950,]
+
+modFDClmrdf.nolog <- FDClmrdf.nolog[! is.na(FDClmrdf.nolog$site),]
+modFDClmrdf.nolog <- modFDClmrdf.nolog[! is.na(FDClmrdf.nolog$min), ]
+modFDClmrdf.nolog <- modFDClmrdf.nolog[FDClmrdf.nolog$decade >= 1950,]
+
+ModelSitesA <- unique(modFDClmrdf.log$site)
+ModelSitesB <- unique(modFDClmrdf.nolog$site)
+if(length(ModelSitesA) != length(ModelSitesB)) {
+   stop("SOMETHING IS WRONG, STOP THE PROJECT!")
+}
+
+ModelSites <- ModelSitesA; rm(ModelSitesA); rm(ModelSitesB)
+
+ModTFDC <- list2env(akq_rm(ModelSites, TFDC, invert=TRUE))
+ModFDCtrend <- visFDCtrend(ModTFDC, file="mod_fdc_trend.pdf")
+save(ModTFDC, ModFDCtrend, file="ModFDCtrend.RData")
+
+
+modFDCLMRlog   <- list2env(akq_rm(ModelSites, FDCLMRlog,   invert=TRUE))
+modFDCLMRnolog <- list2env(akq_rm(ModelSites, FDCLMRnolog, invert=TRUE))
+
+if(length(ls(modFDCLMRlog)) != length(ls(modFDCLMRnolog)) ) {
+   stop("SOMETHING IS WRONG, STOP THE PROJECT!")
+}
+
+save(modFDCLMRlog, modFDCLMRnolog, modFDClmrdf.log, modFDClmrdf.nolog, file="modFDCLMR.RData")
+load("modFDCLMR.RData")
+
+
+ModelSites <- data.frame(site_no=ModelSites)
+write.table(ModelSites, file="ModSiteList.txt", row.names=FALSE)
+
+
+stop()  # experimental material follows below
+
 #pdf("massive.pdf")
 FF <- (1:365)/366; qFF <- qnorm(FF)
-plot(qnorm(c(1/366, 365/366)), c(1E-3, 1000), type="n",
-     xlab="STANDARD NORMAL VARIATE", ylab="log10(cfs+1cfs)")
-lmrDF <- FDClmrdf.nolog
-for(i in sample(1:length(lmrDF$site), size=10, replace=FALSE)) {
+plot(qnorm(c(1/366, 365/366)), c(1E-2, 1E5), type="n", log="y",
+     xlab="STANDARD NORMAL VARIATE", ylab="cfs")
+lmrDF <- modFDClmrdf.nolog
+for(i in (1:length(lmrDF$site))) {
    if(is.na(lmrDF$L1[i])) next
    lmr <- c(lmrDF$L1[i], lmrDF$L2[i],
             lmrDF$T3[i], lmrDF$T4[i], lmrDF$T5[i])
    lmr <- vec2lmom(lmr, checklmom=FALSE)
    if(! are.lmom.valid(lmr)) next;
-   par <- lmom2par(lmr, type="wak")
-   col <- ifelse(par$ifail == 2, rgb(0,0,1,.4),
-          ifelse(par$ifail == 3, rgb(0,1,0,.4), rgb(1,0,0, .4)))
+   par <- lmom2par(lmr, type="gno")
+   col <- rgb(0,0,1,.2)
+   #col <- ifelse(par$ifail == 2, rgb(0,0,1,.4),
+   #       ifelse(par$ifail == 3, rgb(0,1,0,.4), rgb(1,0,0, .4)))
    lines(qFF, qlmomco(FF, par), lwd=0.4, col=col)
 }
 
@@ -64,8 +102,8 @@ for(i in sample(1:length(lmrDF$site), size=10, replace=FALSE)) {
 FF <- (1:365)/366; qFF <- qnorm(FF)
 plot(qnorm(c(1/366, 365/366)), c(1E-3, 10), type="n",
      xlab="STANDARD NORMAL VARIATE", ylab="log10(cfs+1cfs)")
-lmrDF <- FDClmrdf.log
-for(i in sample(1:length(lmrDF$site), size=10000, replace=FALSE)) {
+lmrDF <- modFDClmrdf.log
+for(i in 1:length(lmrDF$site)) {
    if(is.na(lmrDF$L1[i])) next
    lmr <- c(lmrDF$L1[i], lmrDF$L2[i],
             lmrDF$T3[i], lmrDF$T4[i], lmrDF$T5[i])
@@ -79,8 +117,6 @@ for(i in sample(1:length(lmrDF$site), size=10000, replace=FALSE)) {
 
 #dev.off()
 
-
-stop()  # experimental material follows below
 
 for(ktsites in unique(AllFDCtrend$site)) {
    X <- AllFDCtrend[AllFDCtrend$site == ktsites,]
