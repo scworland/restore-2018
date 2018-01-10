4b13225713a90df70950840c748551fe29a2cb54
wasquith-usgs <wasquith@usgs.gov>
2018-01-10 16:45:43 -0600

    daily progress

--- scripts/Asquith/fdc_model/fdcest.R
+++ scripts/Asquith/fdc_model/fdcest.R
@@ -7,10 +7,11 @@ library(rgeos)
 library(lmomco)
 library(Lmoments)
 
-FDC <- read_feather(file.choose()) # "fdc_lmr_pplo.feather" or "fdc_lmr_pplo2010-15.feather"
-# I am using fdc_lmr_pplo2010-15.feather for the testing here.
+FDC <- read_feather(file.choose()) # "all_gage_data.feather"
+load(file.choose()) # "spRESTORE_MGCV_BND.RData"
+load(file.choose()) # spDNI_1998to2009.RData
 
-sites <- unique(FDC$site)
+sites <- unique(FDC$site_no)
 sitefile <- dataRetrieval::readNWISsite(sites)
 sitefile <- sitefile[sitefile$agency_cd != "USCE",]
 
@@ -18,7 +19,7 @@ CDA <- sitefile$contrib_drain_area_va
 CDA[is.na(CDA)] <- sitefile$drain_area_va[is.na(CDA)]
 sitefile$contrib_drain_area_va <- CDA
 
-load(file.choose()) # "spRESTORE_MGCV_BND.RData"
+
 LATLONG <- paste0("+init=epsg:4269 +proj=longlat +ellps=GRS80 ",
                   "+datum=NAD83 +no_defs +towgs84=0,0,0")
 LATLONG <- sp::CRS(LATLONG)
@@ -26,37 +27,37 @@ ALBEA <- paste0("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +
                 "+datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
 ALBEA <- sp::CRS(ALBEA)
 
-D <- data.frame(site=sitefile$site_no,
-                dec_lat_va=sitefile$dec_lat_va,
-                dec_long_va=sitefile$dec_long_va,
-                CDA=log10(CDA), stringsAsFactors=FALSE)
-D <- merge(FDC, D, all=TRUE)
-D <- D[complete.cases(D),]
+DD <- data.frame(site_no=sitefile$site_no,
+                 dec_lat_va=sitefile$dec_lat_va,
+                 dec_long_va=sitefile$dec_long_va,
+                 CDA=log10(CDA), stringsAsFactors=FALSE)
+DD <- merge(FDC, DD, all=TRUE)
+DD <- DD[complete.cases(DD),]
 
-D <- SpatialPointsDataFrame(cbind(D$dec_long_va, D$dec_lat_va), D,
+DD <- SpatialPointsDataFrame(cbind(DD$dec_long_va, DD$dec_lat_va), DD,
                             proj4string=LATLONG)
-D <- spTransform(D, ALBEA)
-XY <- coordinates(D)
-D$east <- XY[,1]/1000; D$north <- XY[,2]/1000; rm(XY)
+DD <- spTransform(DD, ALBEA)
+XY <- coordinates(DD)
+DD$east <- XY[,1]/1000; DD$north <- XY[,2]/1000; rm(XY)
 
-D$prob_has_noflow <- 0
-D$prob_has_noflow[D$nzero > 0] <- 1
+DD$prob_has_noflow <- 0
+DD$prob_has_noflow[DD$nzero > 0] <- 1
 
-load(file.choose()) # spDNI_1998to2009.RData
-SO <- over(D, spDNI_1998to2009)
-D$ANN_DNI <- SO$ANN_DNI
-D$JAN <- SO$JAN; D$FEB <- SO$FEB
-D$MAR <- SO$MAR; D$APR <- SO$APR
-D$MAY <- SO$MAY; D$JUN <- SO$JUN
-D$JUL <- SO$JUL; D$AUG <- SO$AUG
-D$SEP <- SO$SEP; D$OCT <- SO$OCT
-D$NOV <- SO$NOV; D$DEC <- SO$DEC
+
+SO <- over(DD, spDNI_1998to2009)
+DD$ANN_DNI <- SO$ANN_DNI
+DD$JAN <- SO$JAN; DD$FEB <- SO$FEB
+DD$MAR <- SO$MAR; DD$APR <- SO$APR
+DD$MAY <- SO$MAY; DD$JUN <- SO$JUN
+DD$JUL <- SO$JUL; DD$AUG <- SO$AUG
+DD$SEP <- SO$SEP; DD$OCT <- SO$OCT
+DD$NOV <- SO$NOV; DD$DEC <- SO$DEC
 rm(SO)
 
 bnd <- list(x=bnd_poly_aea[,1]/1000, y=bnd_poly_aea[,2]/1000)
 bnd <- list(bnd)
 plot(bnd[[1]]$x,bnd[[1]]$y,type="l", col=8, lwd=.6)
-points(D$east, D$north, pch=4, lwd=.5, cex=0.9, col=8)
+points(DD$east, DD$north, pch=4, lwd=.5, cex=0.9, col=rgb(0,.5,0.5,.2))
 
 # This could be one way to build a grid of knots. However, a
 # large number do not seem to be necessary. Further, knots in
@@ -84,10 +85,35 @@ x <- knots$x; y <- knots$y
 knots <- data.frame(x=c(x, 1375), y=c(y, 600)); rm(x,y)
 points(knots$x, knots$y, pch=16, cex=1.1, col=4)
 
+knots_pplo <- knots[-17, ]
+
 # The as.numeric() is needed head of pplo use later
 plogit <- function(eta) as.numeric(exp(eta)/(exp(eta)+1))
 duan_smearing_estimator <- function(model) { sum(10^residuals(L1))/length(residuals(L1)) }
 
+DD$x <- DD$east; DD$y <- DD$north
+DD$flood_storage <- (DD$tot_nid_storage - DD$tot_norm_storage)/10^DD$CDA
+DD[DD$flood_storage < 0,] # two sites: 02295420 and 02296750
+DD$flood_storage[DD$flood_storage < 0] <- (DD$tot_norm_storage[DD$flood_storage < 0] -
+                                            DD$tot_nid_storage[DD$flood_storage < 0]) /
+                                                     10^DD$CDA[DD$flood_storage < 0]
+DD$tot_basin_slope <- log10(DD$tot_basin_slope)
+DD$decade <- as.factor(DD$decade)
+DD$aquifers <- as.factor(DD$aquifers)
+mf <- (DD$mixed_forest/100)/(1-DD$mixed_forest/100)
+D <- DD;
+
+# [45] "ppt_mean"            "ppt_sd"              "temp_mean"           "temp_sd"
+# [49] "tot_hdens"           "tot_major"           "tot_ndams"           "tot_nid_storage"
+# [53] "tot_norm_storage"    "barren"              "cultivated_cropland" "deciduous_forest"
+# [57] "developed"           "evergreen_forest"    "grassland"           "hay_pasture"
+# [61] "herbaceous_wetland"  "mixed_forest"        "perennial_ice_snow"  "shrubland"
+# [65] "water"               "woody_wetland"       "tot_bfi"             "sinuosity"
+# [69] "length_km"           "area_sqkm"           "strm_dens"           "tot_twi"
+# [73] "tot_basin_area"      "tot_basin_slope"     "tot_elev_mean"       "tot_elev_min"
+# [77] "tot_elev_max"        "tot_total_road_dens" "tot_rdx"             "bedperm"
+# [81] "aquifers"            "soller"              "hlr"                 "ecol3"
+# [85] "physio"              "statsgo"
 
 # It appears critical that the boundary have variables named say x,y
 # The knots have the same names (x,y) and most difficult to figure out
@@ -95,34 +121,39 @@ duan_smearing_estimator <- function(model) { sum(10^residuals(L1))/length(residu
 # the critical piece, it is that they are all the same literal string.
 # For example, v,w would work too.
 Z <- D[D$nzero > 0,]
-x <- Z$east; y <- Z$north # Please see the extraction and setting to x,y
-CDA <- Z$CDA; ANN_DNI <- Z$ANN_DNI; MAY <- Z$MAY; DEC <- Z$DEC; z <- Z$pplo
-PPLO <- gam(z~s(CDA, bs="cr")+s(ANN_DNI, bs="cr")+
+#x <- Z$east; y <- Z$north # Please see the extraction and setting to x,y
+#CDA <- Z$CDA; ANN_DNI <- Z$ANN_DNI; MAY <- Z$MAY; DEC <- Z$DEC
+Z$z <- D$pplo; Z$mf <- mf
+PPLO <- gam(z~s(CDA, bs="cr", k=6)+s(ANN_DNI, bs="cr", k=6)+
+              te(ppt_mean, temp_mean)+mf+
               s(MAY, DEC, bs="tp", k=6)+
-              s(x, y, bs="so", xt=list(bnd=bnd)), knots=knots,
-              family="quasibinomial")
+              s(x, y, bs="so", xt=list(bnd=bnd))+
+              developed,
+              knots=knots_pplo, data=Z, family="quasibinomial")
 pdf("PPLO.pdf", useDingbats=FALSE)
-  plot(ANN_DNI, z, pch=16, col=8);
-  points(ANN_DNI, fitted.values(PPLO))
+  plot(Z$ANN_DNI, Z$z, pch=16, col=8)
+  points(Z$ANN_DNI, fitted.values(PPLO))
   plot(PPLO, scheme=2)
-  points(x, y, pch=4, lwd=.5, cex=0.9, col=8)
-  points(knots$x, knots$y, pch=16, cex=1.1, col=4)
+  points(Z$x, Z$y, pch=4, lwd=.5, cex=0.9, col=8)
+  points(knots_pplo$x, knots_pplo$y, pch=16, cex=1.1, col=4)
   text(100, 500, "Probability level of noflows", pos=4)
 dev.off()
 
 
-x <- D$east; y <- D$north
-CDA <- D$CDA; ANN_DNI <- D$ANN_DNI; MAY <- D$MAY; DEC <- D$DEC
-z <- D$prob_has_noflow
-ZERO <- gam(z~s(CDA, bs="cr")+s(ANN_DNI, bs="cr")+
-              s(x, y, bs="so", xt=list(bnd=bnd)), knots=knots,
-              family="quasibinomial")
+Z <- D
+z <- Z$prob_has_noflow
+ZERO <- gam(z~s(CDA, bs="cr", k=6)+s(ANN_DNI, bs="cr", k=6)+
+              te(ppt_mean, temp_mean)+
+              s(MAY, DEC, bs="tp", k=6)+
+              s(x, y, bs="so", xt=list(bnd=bnd))+
+              developed,
+              knots=knots_pplo, data=Z, family="quasibinomial")
 pdf("ZERO.pdf", useDingbats=FALSE)
-  plot(ANN_DNI, z, pch=16, col=8);
-  points(ANN_DNI, fitted.values(ZERO))
+  plot(Z$ANN_DNI, z, pch=16, col=8);
+  points(Z$ANN_DNI, fitted.values(ZERO))
   plot(ZERO, scheme=2)
   points(x, y, pch=4, lwd=.5, cex=0.9, col=8)
-  points(knots$x, knots$y, pch=16, cex=1.1, col=4)
+  points(knots_pplo$x, knots_pplo$y, pch=16, cex=1.1, col=4)
   text(100, 500, "Probability model of site having at least one noflow", pos=4)
 dev.off()
 
