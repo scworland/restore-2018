a22967245d1948bbed714148ca6450051c81f6fc
scworland-usgs <scworland@usgs.gov>
2018-07-03 10:20:20 -0400

    updated remake scripts

--- scripts/Worland/remake_scripts/correlation_analysis.R
+++ scripts/Worland/remake_scripts/correlation_analysis.R
@@ -1,53 +1,57 @@
-library(tidyverse)
-library(feather)
-library(corrr)
 
-source("scripts/Worland/utils.R")
-
-# load data
-d <- remake::fetch('gage_all')
-
-Y <- select(d,f50) %>%
-  mutate_all(funs(.+2)) %>%
-  mutate_all(funs(log10))
-
-X <- select(d,lon,lat,acc_hdens:statsgo,-perennial_ice_snow,-area_sqkm) %>%
-  mutate_at(vars(bedperm:statsgo),funs(as.numeric(as.factor(.)))) %>%
-  mutate_all(funs(as.vector(scale(.)))) %>%
-  select_if(~!any(is.na(.)))
-
-# correlation analysis with quantile
-corrr_analysis <- X %>%
-  mutate(y = Y$f50) %>%
-  correlate() %>%
-  focus(y) %>%
-  rename(feature = rowname) %>%
-  arrange(abs(y)) %>%
-  mutate(feature = as.factor(feature))
-
-corrr_analysis %>%
-  ggplot(aes(x = y, y = fct_reorder(feature, desc(y)))) +
-  geom_point() +
-  geom_segment(aes(xend = 0, yend = feature), 
-               color = palette_light()[[2]], 
-               data = corrr_analysis %>% filter(y > 0)) +
-  geom_point(color = palette_light()[[2]], 
-             data = corrr_analysis %>% filter(y > 0)) +
-  geom_segment(aes(xend = 0, yend = feature), 
-               color = palette_light()[[1]], 
-               data = corrr_analysis %>% filter(y < 0)) +
-  geom_point(color = palette_light()[[1]], 
-             data = corrr_analysis %>% filter(y < 0)) +
-  labs(x="correlation with 50th percentile",y="features") +
-  theme_bw()
-
-# correlation EDA
-x_cor <- X %>%
-  #select(barren:woody_wetland) %>%
-  correlate() %>% 
-  shave() %>%
-  stretch() %>%
-  arrange(desc(abs(r))) %>%
-  na.omit()
-
-rplot(x_cor)
+sw_plot_corr <- function(gage_all){
+  
+  # source("scripts/Worland/utils.R")
+  
+  # load data
+  d <- gage_all
+  
+  f15 <- c("f0.02","f0.5","f05","f10","f20", "f30","f40","f50",
+           "f60","f70","f80","f90","f95","f99.5","f99.98")
+  
+  Y <- select(d,f15) %>%
+    mutate_all(funs(.+2)) %>%
+    mutate_all(funs(log10))
+  
+  X <- select(d,major:flood_storage) %>%
+    mutate_all(funs(as.numeric(as.factor(.)))) %>%
+    mutate_all(funs(as.vector(scale(.)))) %>%
+    select_if(~!any(is.na(.)))  %>% 
+    as.matrix()
+  
+  # correlation analysis with quantile
+  corrr_analysis <- X %>%
+    mutate(f0.02 = Y$f0.02,
+           #f50 = Y$f50,
+           f99.98 = Y$f99.98) %>%
+    correlate() %>%
+    focus(f0.02,f99.98) %>%
+    mutate(mu = mean(c(f0.02,f99.98))) %>%
+    rename(feature = rowname) %>%
+    arrange(abs(mu)) %>%
+    select(-mu) %>%
+    mutate(feature = as.factor(feature)) %>%
+    gather(quantile,value,-feature) %>%
+    mutate(sign = ifelse(value>0,"pos","neg"))
+  
+  corrr_analysis %>%
+    ggplot(aes(x = value, y = fct_reorder(feature, desc(value)))) +
+    geom_segment(aes(xend = 0, yend = feature,color=sign)) +
+    geom_vline(xintercept=0,linetype="dashed") +
+    coord_cartesian(xlim=c(-0.8,0.8)) +
+    geom_point(aes(shape=quantile,color=sign),fill="white") +
+    scale_color_manual(values=c("blue","red"),guide=F) +
+    scale_shape_manual(values=c(21,22)) +
+    labs(x="correlation",y="features") +
+    theme_bw()
+  
+  # # correlation EDA
+  # x_cor <- X %>%
+  #   #select(barren:woody_wetland) %>%
+  #   correlate() %>% 
+  #   focus(acc_basin_area) %>%
+  #   arrange(desc(abs(acc_basin_area))) %>%
+  #   na.omit()
+  # 
+  # rplot(x_cor)
+}
--- scripts/Worland/remake_scripts/quantile_number_violations.R
+++ scripts/Worland/remake_scripts/quantile_number_violations.R
@@ -1,109 +1,100 @@
-library(keras)
-library(tidyverse)
-library(feather)
-library(broom)
-library(zoo)
-source("scripts/Worland/utils.R")
-source("scripts/Worland/keras_funs.R")
 
-# setwd("~/Documents/Restore")
-d <- remake::fetch('gage_all')
+sw_count_violations <- function(gage_all){
 
-Y <- select(d,f0.02:f99.98) %>%
-  mutate_all(funs(.+2)) %>%
-  mutate_all(funs(log10))
-
-X <- select(d,lon,lat,acc_hdens:statsgo,-perennial_ice_snow,-area_sqkm) %>%
-  mutate_at(vars(bedperm:statsgo),funs(as.numeric(as.factor(.)))) %>%
-  mutate_all(funs(as.vector(scale(.)))) %>%
-  select_if(~!any(is.na(.))) %>% 
-  as.matrix()
-
-f1 <- c("f0.02","f0.05","f0.1","f0.2","f0.5","f01","f02","f05",
-        "f10","f20","f25","f30","f40","f50","f60","f70","f75",
-        "f80","f90","f95","f98","f99","f99.5","f99.8","f99.9",
-        "f99.95","f99.98")
-
-f2 <- c("f0.02","f0.1","f0.5","f02","f05","f10","f20","f25",
-        "f30","f40","f50","f60","f70","f75","f80","f90","f95",
-        "f98","f99.5","f99.9","f99.98")
-
-f3 <- c("f0.02","f0.5","f05","f10","f20", "f30","f40","f50",
-        "f60","f70","f80","f90","f95","f99.5","f99.98")
-
-f4 <- c("f0.02","f0.5","f05","f10","f20", "f30","f40","f50",
-        "f60","f70","f80","f90","f95","f99.5","f99.98")
-
-f5 <- c("f05","f10","f20", "f30","f40","f50","f60","f70",
-        "f80","f90","f95")
-
-f6 <- c("f05","f20", "f30","f40","f50","f60","f70","f80","f95")
-
-f_all <- list(f1,f2,f3,f4,f5,f6)
-
-viol_all <- NULL
-for (i in 1:length(f_all)) {
+  d <- gage_all
   
-  print(paste0("Analyzing Scenario ",i," out of ", length(f_all)))
+  Y <- select(d,f0.02:f99.98) %>%
+    mutate_all(funs(.+2)) %>%
+    mutate_all(funs(log10))
+
+  X <- select(d,major:flood_storage) %>%
+    mutate_all(funs(as.numeric(as.factor(.)))) %>%
+    mutate_all(funs(as.vector(scale(.)))) %>%
+    select_if(~!any(is.na(.)))  %>% 
+    as.matrix()
   
-  # subset Y matrix
-  Y2 <- Y[,f_all[[i]]]
+  f27 <- c("f0.02","f0.05","f0.1","f0.2","f0.5","f01","f02","f05",
+          "f10","f20","f25","f30","f40","f50","f60","f70","f75",
+          "f80","f90","f95","f98","f99","f99.5","f99.8","f99.9",
+          "f99.95","f99.98")
   
-  # Build model function for multiple outputs
-  build_model <- function(){
-    input <- layer_input(shape=dim(X)[2],name="basinchars")
+  f21 <- c("f0.02","f0.1","f0.5","f02","f05","f10","f20","f25",
+          "f30","f40","f50","f60","f70","f75","f80","f90","f95",
+          "f98","f99.5","f99.9","f99.98")
+  
+  f15 <- c("f0.02","f0.5","f05","f10","f20", "f30","f40","f50",
+          "f60","f70","f80","f90","f95","f99.5","f99.98")
+  
+  f11 <- c("f05","f10","f20", "f30","f40","f50","f60","f70",
+          "f80","f90","f95")
+  
+  f9 <- c("f05","f20", "f30","f40","f50","f60","f70","f80","f95")
+  
+  f_all <- list(f27,f21,f15,f11,f9)
+  
+  viol_all <- NULL
+  for (i in 1:length(f_all)) {
     
-    base_model <- input  %>%
-      layer_dense(units = 40,activation="relu") %>%
-      layer_dropout(rate=0.1) %>%
-      layer_dense(units = 20,activation="relu") %>%
-      layer_dropout(rate=0.1) %>%
-      layer_dense(units = 20,activation="relu") %>%
-      layer_dropout(rate=0.1) 
+    print(paste0("Analyzing Scenario ",i," out of ", length(f_all)))
     
-    for(j in 1:dim(Y2)[2]){
-      y <- colnames(Y2)[j]
-      outstring <- paste0(
-        sprintf("%s <- base_model %%>%%", y), 
-        sprintf(" layer_dense(units = 1, activation='relu', name='%s')",y)
-      )
-      eval(parse(text=outstring))
+    # subset Y matrix
+    Y2 <- Y[,f_all[[i]]]
+    
+    # Build model function for multiple outputs
+    build_model <- function(){
+      input <- layer_input(shape=dim(X)[2],name="basinchars")
+      
+      base_model <- input  %>%
+        layer_dense(units = 25,activation="relu") %>%
+        layer_dropout(rate=0.558) %>%
+        layer_dense(units = 49,activation="relu") %>%
+        layer_dropout(rate=0.591) 
+      
+      for(j in 1:dim(Y2)[2]){
+        y <- colnames(Y2)[j]
+        outstring <- paste0(
+          sprintf("%s <- base_model %%>%%", y), 
+          sprintf(" layer_dense(units = 1, activation='relu', name='%s')",y)
+        )
+        eval(parse(text=outstring))
+      }
+      
+      Ylist <- paste0("list(",paste(colnames(Y2),sep="",collapse=","),")")
+      model <- keras_model(input,eval(parse(text=Ylist))) %>%
+        compile(optimizer_rmsprop(lr = 0.002536862),
+                loss="mse",
+                metrics="mae")
+      
+      return(model)
     }
     
-    Ylist <- paste0("list(",paste(colnames(Y2),sep="",collapse=","),")")
-    model <- keras_model(input,eval(parse(text=Ylist))) %>%
-      compile(optimizer = "rmsprop",
-              loss="mse",
-              metrics="mae")
+    # K-fold cross validation
+    epochs <- 250
+    cv_results <- sw_k_foldcv(build_model,k=5,epochs=epochs,batch_size=294,Y=Y2,X=X,data=d)
     
-    return(model)
+    viol_count <- data.frame(t(cv_results$violations)) %>%
+      set_names(paste0("kfold",1:ncol(.))) %>%
+      mutate(epoch=1:epochs) %>%
+      gather(fold,value,-epoch) %>%
+      mutate(quants_est = ncol(Y2))
+    
+    viol_all <- rbind(viol_all,viol_count)
   }
   
-  # K-fold cross validation
-  epochs <- 200
-  cv_results <- k_foldcv(k=5,epochs=epochs,batch_size=100,Y=Y2,X=X,data=d,pls=FALSE)
-  
-  viol_count <- data.frame(t(cv_results$violations)) %>%
-    set_names(paste0("kfold",1:ncol(.))) %>%
-    mutate(epoch=1:epochs) %>%
-    gather(fold,value,-epoch) %>%
-    mutate(quants_est = paste0(ncol(Y2), "_quantiles"))
-  
-  viol_all <- rbind(viol_all,viol_count)
+  return(viol_all)
+
+  # viol_count <- viol_all %>%
+  #   group_by(epoch,quants_est,quants_est) %>%
+  #   summarize(min=min(value),
+  #             max=max(value),
+  #             mu=mean(value)) %>%
+  #   mutate(quants_est = factor(quants_est,quants_est))
+  # 
+  # 
+  # ggplot(viol_count) +
+  #   geom_line(aes(x=epoch,y=mu,color=quants_est, group = quants_est)) +
+  #   scale_color_viridis_d(begin=0.1,end=0.8,option="C") +
+  #   coord_cartesian(xlim=c(0,150)) +
+  #   theme_bw() +
+  #   labs(color="#quantiles est", fill = "#quantiles est", y="number of violations")
 }
-
-viol_count <- viol_all %>%
-  group_by(epoch,quants_est,quants_est) %>%
-  summarize(min=min(value),
-            max=max(value),
-            mu=mean(value))
-
-
-ggplot(viol_count) +
-  geom_line(aes(x=epoch,y=mu,color=quants_est, group = quants_est)) +
-  #scale_linetype_manual(values = rep("solid",5),guide = FALSE) +
-  coord_cartesian(xlim=c(0,150)) +
-  #scale_y_log10() +
-  theme_bw() +
-  labs(color="#quantiles est", fill = "#quantiles est", y="number of violations")
-
