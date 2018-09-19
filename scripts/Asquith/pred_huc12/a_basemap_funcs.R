8bdbed610106d1528224ca8f80ca67d7b04683f6
wasquith-usgs <wasquith@usgs.gov>
2018-09-19 14:40:03 -0500

    enhancing the placement of things and other labels

--- scripts/Asquith/pred_huc12/a_basemap_funcs.R
+++ scripts/Asquith/pred_huc12/a_basemap_funcs.R
@@ -1,3 +1,22 @@
+my.choro.legend <- function(px, py, sh, under="under", over="over", between="to",
+                            fmt="%g", cex=1, ...) {
+    x = sh$breaks
+    lx = length(x)
+    if (lx < 3)
+        stop("break vector too short")
+    res = character(lx + 1)
+    res[1] = paste(under, sprintf(fmt, x[1]))
+    for (i in 1:(lx - 1)) res[i + 1] <- paste(paste0(sprintf(fmt, x[i]),"1"), # WHA hack
+        between, sprintf(fmt, x[i + 1]))
+    res[lx + 1] <- paste(over, paste0(sprintf(fmt, x[lx]),"1")) # WHA hack
+    maxwidth <- max(strwidth(res))
+    temp <- legend(x = px, y = py, legend = rep(" ", length(res)),
+        fill = sh$cols, text.width = maxwidth, cex = cex, ...)
+    text(temp$rect$left + temp$rect$w, temp$text$y, res, pos = 2,
+        cex = cex)
+}
+
+
 
 map_annotation <- function() {
   ss <- list(x=-90000, y=320000)
@@ -16,19 +35,19 @@ map_annotation <- function() {
 
   txt <- paste0("Albers Equal Area Projection\n",
                 "North American Datum of 1983\n",
-                "Base modified from U.S. Geological Survey digital data, 1:24,000")
-  text(-420000, 1590000, txt, pos=4, cex=0.45)
+                "Base modified from USGS digital data, 1:24,000")
+  text(145000, 669000, txt, pos=4, cex=0.45)
   plot(GulfStates_modified, add=TRUE, lwd=.4, lty=2)
   STATES <- c("Texas", "Oklahoma", "Missouri", "Arkansas", "Louisiana", "Mississippi",
               "Tennessee", "Kentucky", "Alabama", "Georgia", "Florida")
-  STATES <- data.frame(easting=c(-440000, -202900.4,  178000,  178000,  279961.5,
-                                 430000,  710000,  710000,  710000,
+  STATES <- data.frame(easting=c(-410000, -202900.4,  178000,  178000,  400000,
+                                 490000,  740000,  740000,  740000,
                                  1100000, 1290000),
-                       northing=c(955139.0, 1400000, 1558716.4, 1400000,  795368.5,
-                                  1170000, 1450000, 1600000, 1325000,
-                                  1170000, 800000),
+                       northing=c(955139.0, 1400000, 1558716.4, 1400000,  795000,
+                                  1165000, 1450000, 1600000, 1325000,
+                                  1165000, 800000),
                        state=STATES)
-  text(STATES$easting, STATES$northing, STATES$state, pos=4, cex=0.7, col=grey(0.3))
+  text(STATES$easting, STATES$northing, STATES$state, pos=4, cex=0.8, col=grey(0.3))
   plot(GL, lwd=0.4, col=grey(0.22), add=TRUE)
 }
 
@@ -82,24 +101,39 @@ choropleth_cov <-
     return(shades)
   }
 
-legend_est <- function(gage="", title="", note=TRUE, shades=NA, itgage=TRUE, ...) {
-  tx1 <- paste0("U.S. Geological Survey streamflow-gaging station:\n",
-                "Symbol colored* by observed decadal ",gage)
-  tx2 <- paste0("'COMID' location in the National Hydrography Dataset\n",
-                "version 2: Symbol colored by estimated decadal\n",gage)
+legend_est <- function(gage="", title="", note=TRUE, shades=NA, itgage=TRUE, more=NA, ...) {
+  sa  <- paste0("Boundary of study as defined by Crowley-Ornelas and others (2018a)")
+  tx1 <- paste0("USGS streamgage and symbol* colored according to the legend to the right")
+  tx2 <- paste0("Prediction location on NHD+ network colored according to the legend to the right")
+
+  #tx2 <- paste0("'COMID' location in the National Hydrography Dataset\n",
+  #              "version 2: Symbol colored by estimated decadal\n",gage)
+  xx <- -80000
   if(itgage) {
-    legend(-80000, 595000, c(tx1, tx2), bty="n", cex=0.7, pt.cex=c(0.8,0.6),
-           lwd=c(0.7,1), lty=c(0,0), pch=c(1,16), col=c("#3288BDE6","#D53E4FE6"), ...)
+    legend(xx, 530000, c(sa, tx1, tx2), bty="n", cex=0.7, pt.cex=c(NA,0.8,0.6),
+           lwd=c(0.7,0.7,1), lty=c(1,0,0), pch=c(NA,1,16), col=c(1,"#3288BDE6","#D53E4FE6"), ...)
   } else {
-    legend(-80000, 520000, c(tx2), bty="n", cex=0.7, pt.cex=c(0.6),
-           lwd=c(1), lty=c(0), pch=c(16), col=c("#D53E4FE6"), ...)
+    legend(xx, 520000, c(sa, paste0("USGS streamgage and symbol* size changes"), tx2), bty="n",
+           cex=0.7, pt.cex=c(NA,0.8,0.6),
+           lwd=c(0.7,0.7,1), lty=c(1,0,0), pch=c(NA,1,16), col=c(1,grey(0.72),"#D53E4FE6"), ...)
   }
-  choro.legend(850000, 690000, shades, cex=0.7, bty="n", box.col=grey(1), bg=grey(1),
+  my.choro.legend(895000, 720000, shades, cex=0.7, bty="n", box.col=grey(1), bg=grey(1),
                fmt="%g", xjust=0, title=title)
   if(note) {
-    text(270000, 320000,
-         "* Note that symbol size represents decade\n (1950 [smallest circle] through 2000 [largest circle]).",
+    text(260000, 380000,
+         paste0("* Note that symbol size represents one the six decades:\n     ",
+                "1950s (smallest circle) through 2000s (largest circle)."),
          cex=.6, pos=4)
+    abb <- paste0("Abbreviations: ",
+                  "USGS, U.S. Geological Survey",
+                  "; NHD+, National Hydrolgraph Dataset plus version 2,\n     ",
+                  "locations specific to Crowley-Ornelas and others (2018c,d) ")
+    if(! is.na(more)) {
+      abb <- paste0(abb,more)
+      text(260000, 290000, abb, cex=0.6, pos=4)
+    } else {
+      text(260000, 300000, abb, cex=0.6, pos=4)
+    }
   }
 }
 
