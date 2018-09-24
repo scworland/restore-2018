ace36799258efe29f195922f2abffad811cdc92b
wasquith-usgs <wasquith@usgs.gov>
2018-09-24 15:25:04 -0500

    fresh audit as we finish up the data releases

--- scripts/Asquith/sitefile/active_discontinued.R
+++ scripts/Asquith/sitefile/active_discontinued.R
@@ -1,16 +1,18 @@
+library(akdecay)
 #file <- file.choose() # go find full_site_list.csv or decade1950plus_site_list.csv
 #sites <- read.table(file, header=TRUE, colClasses="character")
 #sites <- sites$site_no
-#DV <- new.env()
-#fill_dvenv(sites, envir=DV, edate="2016-09-30", ignore.provisional=TRUE)
-#save(DV, file="DVbangP.RData")
+DV <- new.env()
+fill_dvenv(sites, envir=DV, edate="2017-09-30", ignore.provisional=TRUE)
+save(DV, file="DVbangP.RData")
 
 
-beg <- end <- n <- vector(mode="numeric", length(ls(DV)))
+beg <- end <- n <- rep(NA, length(ls(DV)))
 i <- 0; sites <- sort(ls(DV))
 for(i in 1:length(sites)) {
    tmp <- get(sites[i], envir=DV)
    yy <- range(tmp$year)
+   if(! is.finite(yy[1])) next
    beg[i] <- yy[1]
    end[i] <- yy[2]
    n[i] <- length(tmp$Flow)
@@ -19,7 +21,8 @@ zz <- data.frame(site_no=sites, count_dvs=n,
                  approx_por_pct=as.integer(100*n/((end-beg)*365.25)),
                  begin_cal_year=beg, end_cal_year=end)
 zz$active <- FALSE
-zz$active[zz$end_cal_year == 2016] <- TRUE
+zz$active[zz$end_cal_year == 2017] <- TRUE
+zz$active[is.na(zz$count_dvs)] <- FALSE
 
 ExtraSiteFileStuff <- zz
 write.table(ExtraSiteFileStuff, file="ExtraSiteFileStuff.txt", quote=TRUE, row.names=FALSE, sep="\t")
