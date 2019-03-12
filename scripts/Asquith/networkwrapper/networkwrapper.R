0714533e15fe2e1eeabd5723f6ad9a90245c592b
wasquith-usgs <wasquith@usgs.gov>
2019-03-12 15:58:05 -0500

    update

--- scripts/Asquith/networkwrapper/networkwrapper.R
+++ scripts/Asquith/networkwrapper/networkwrapper.R
@@ -10,8 +10,14 @@ ALBEA <- paste0("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +
                 "+datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
 ALBEA <- CRS(ALBEA)
 
-H   <- read.table("data/all_huc12_covariates.csv", sep=",", header=TRUE,
+H   <- read.table("../../../scibase/huc12/all_huc12_covariates.csv", sep=",", header=TRUE,
                   colClasses="character")
+length(H$comid)                           # [1] 55320 (20190311)
+
+#SOL <- read.table("../../../scibase/huc12/all_huc12_solar.csv", sep=",", header=TRUE,
+#                    colClasses="character")
+#length(SOL$comid)                         # [1] 55128 (20190311)
+# # ScienceBase is missing rows (55320-55128)/6 --- > 32 gages
 SOL <- read_feather("../../../data/huc12/all_huc12_solar.feather")
 H <- merge(H,SOL)
 
@@ -23,11 +29,12 @@ H$elev_mean   <- log10(as.numeric(H$elev_mean)+100)
 H$ppt_mean    <- log10(as.numeric(H$ppt_mean))
 H$decade      <- as.factor(H$decade)
 
-GAG <- read.table("data/all_gage_covariates.csv", sep=",", header=TRUE,
+GAG <- read.table("../../../scibase/gage/all_gage_covariates.csv", sep=",", header=TRUE,
                   colClasses="character")
-STA <- read.table("data/all_gage_flow_stats.csv", sep=",", header=TRUE,
+STA <- read.table("../../../scibase/gage/all_gage_flow_stats.csv", sep=",", header=TRUE,
                   colClasses="character")
 SOL <- read_feather("../../../data/gage/all_gage_solar.feather")
+
 G <- merge(GAG,STA); rm(GAG); rm(STA)
 G$f90 <- as.numeric(G$f90)
 G$dec_lat_va  <-       as.numeric(G$dec_lat_va)
@@ -58,28 +65,29 @@ G$xkm <- XY[,1]; G$ykm <- XY[,2]; rm(XY)
 
 Z <- log10(G$f90); tmp <- G[is.finite(Z),]; Z <- Z[is.finite(Z)]
 tmp$Z <- Z; rm(Z)
+#tmp <- tmp[tmp$xkm > -186839/1000,] # from locator()
 GAM <-  gam(Z~s(basin_area)+s(basin_slope)+s(elev_mean)+s(ppt_mean)+s(xkm, ykm)+dni_ann+decade, data=tmp)
 SVM <- ksvm(Z~  basin_area +  basin_slope +  elev_mean +  ppt_mean +  xkm+ ykm +dni_ann+decade, data=tmp)
 
 h <- H[H$decade == 2000,]
-#h <- h[h$xkm > -186839/1000,] # from locator()
+h <- h[h$xkm > -186839/1000,] # from locator()
 pGAM <- as.vector(predict(GAM, newdata=h))
 pSVM <- as.vector(predict(SVM, newdata=h))
 
 pdf("networktest_all.pdf", useDingbats=FALSE)
-h$DIFF <- abs(pGAM-pSVM)#/((pGAM+pSVM)/2)
-quas <- quantile(h$DIFF, probs=c(0.90,0.95,0.98))
-plot(H[H$decade == 2000,], col=NA)
-#plot(H[H$decade == 2000 & H$xkm <= -186839/1000,], add=TRUE, pch=1, col=grey(0.8), cex=H$basin_area/4, lwd=0.3)
-plot(H[H$decade == 2000,], add=TRUE, pch=1, col=grey(0.8), cex=H$basin_area/4, lwd=0.3)
-set <- (h$DIFF < quas[1])
-plot(h[set,], add=TRUE, pch=1, col=grey(0.4), cex=h$basin_area/4, lwd=0.3)
-set <- (h$DIFF >= quas[1]) & (h$DIFF < quas[2])
-plot(h[set,], add=TRUE, pch=1, lwd=0.6, col=3, cex=h$basin_area/4)
-set <- (h$DIFF >= quas[2]) & (h$DIFF < quas[3])
-plot(h[set,], add=TRUE, pch=1, lwd=0.6, col=4, cex=h$basin_area/4)
-set <- (h$DIFF >= quas[3])
-plot(h[set,], add=TRUE, pch=1, lwd=0.6, col=2, cex=h$basin_area/4)
+  h$DIFF <- abs(pGAM-pSVM)#/((pGAM+pSVM)/2)
+  quas <- quantile(h$DIFF, probs=c(0.90,0.95,0.98))
+  plot(H[H$decade == 2000,], col=NA)
+  #plot(H[H$decade == 2000 & H$xkm <= -186839/1000,], add=TRUE, pch=1, col=grey(0.8), cex=H$basin_area/4, lwd=0.3)
+  plot(H[H$decade == 2000,], add=TRUE, pch=1, col=grey(0.8), cex=H$basin_area/4, lwd=0.3)
+  set <- (h$DIFF < quas[1])
+  plot(h[set,], add=TRUE, pch=1, col=grey(0.4),  cex=h$basin_area/4, lwd=0.3)
+  set <- (h$DIFF >= quas[1]) & (h$DIFF < quas[2])
+  plot(h[set,], add=TRUE, pch=1, lwd=0.6, col=3, cex=h$basin_area/4)
+  set <- (h$DIFF >= quas[2]) & (h$DIFF < quas[3])
+  plot(h[set,], add=TRUE, pch=1, lwd=0.6, col=4, cex=h$basin_area/4)
+  set <- (h$DIFF >= quas[3])
+  plot(h[set,], add=TRUE, pch=1, lwd=0.6, col=2, cex=h$basin_area/4)
 dev.off()
 
 
