d0a955ed519f2f8ccd4b93204127e0e86ef38267
wasquith-usgs <wasquith@usgs.gov>
2019-03-12 15:34:29 -0500

    updated to 2018 and testing

--- scripts/Asquith/sitefile/active_discontinued.R
+++ scripts/Asquith/sitefile/active_discontinued.R
@@ -1,9 +1,9 @@
-library(akdecay)
+library(akqdecay)
 #file <- file.choose() # go find full_site_list.csv or decade1950plus_site_list.csv
-#sites <- read.table(file, header=TRUE, colClasses="character")
-#sites <- sites$site_no
+sites <- read.table(file, header=TRUE, colClasses="character")
+sites <- sites$site_no
 DV <- new.env()
-fill_dvenv(sites, envir=DV, edate="2017-09-30", ignore.provisional=TRUE)
+fill_dvenv(sites, envir=DV, edate="2018-09-30", ignore.provisional=TRUE)
 save(DV, file="DVbangP.RData")
 
 
@@ -21,7 +21,7 @@ zz <- data.frame(site_no=sites, count_dvs=n,
                  approx_por_pct=as.integer(100*n/((end-beg)*365.25)),
                  begin_cal_year=beg, end_cal_year=end)
 zz$active <- FALSE
-zz$active[zz$end_cal_year == 2017] <- TRUE
+zz$active[zz$end_cal_year == 2018] <- TRUE
 zz$active[is.na(zz$count_dvs)] <- FALSE
 
 ExtraSiteFileStuff <- zz
