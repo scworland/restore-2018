3f6e7542910c9679245e60936b35f33c2d383f3a
wasquith-usgs <wasquith@usgs.gov>
2019-03-13 10:52:50 -0500

    update

--- scripts/Asquith/sitefile/active_discontinued.R
+++ scripts/Asquith/sitefile/active_discontinued.R
@@ -11,6 +11,10 @@ beg <- end <- n <- rep(NA, length(ls(DV)))
 i <- 0; sites <- sort(ls(DV))
 for(i in 1:length(sites)) {
    tmp <- get(sites[i], envir=DV)
+   if(length(tmp$year) == 0) {
+     message(sites[i]," is missing?")
+     next
+   }
    yy <- range(tmp$year)
    if(! is.finite(yy[1])) next
    beg[i] <- yy[1]
@@ -21,7 +25,7 @@ zz <- data.frame(site_no=sites, count_dvs=n,
                  approx_por_pct=as.integer(100*n/((end-beg)*365.25)),
                  begin_cal_year=beg, end_cal_year=end)
 zz$active <- FALSE
-zz$active[zz$end_cal_year == 2018] <- TRUE
+zz$active[zz$end_cal_year >= 2017] <- TRUE
 zz$active[is.na(zz$count_dvs)] <- FALSE
 
 ExtraSiteFileStuff <- zz
